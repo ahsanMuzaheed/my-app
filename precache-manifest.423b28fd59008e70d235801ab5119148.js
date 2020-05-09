9b9a911608155e92912b38c5b46cd39173f2dc0b
ahsanmuzaheed <ahsanmuzaheed@gmail.com>
2020-05-09 07:37:54 +0200

    Updates

---
+++ precache-manifest.423b28fd59008e70d235801ab5119148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045ec24f866b5b048f91ad98693ce058",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2871fd8427f7425f2488",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85a753778bcb8779e80d",
+    "url": "/my-app/static/js/2.c8653838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.c8653838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2871fd8427f7425f2488",
+    "url": "/my-app/static/js/main.4d94798b.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
